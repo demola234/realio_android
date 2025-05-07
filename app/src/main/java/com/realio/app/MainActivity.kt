1ca07830d63ff7f169aefb9370ca765bea8ed6ae
demola234 <kolawoleoluwasegun567@gmail.com>
2025-05-07 13:58:55 +0100

    feat: add initial project files and splash screen setup

--- app/src/main/java/com/realio/app/MainActivity.kt
+++ app/src/main/java/com/realio/app/MainActivity.kt
@@ -11,10 +11,23 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.realio.app.ui.theme.RealioTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
+        splashScreen.setKeepOnScreenCondition {true}
+
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(2000)
+            splashScreen.setKeepOnScreenCondition {false}
+        }
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
